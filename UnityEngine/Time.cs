ae7a68a1f0007eedabb6dede46532dc08a9b3150
Hoxily <hoxily@qq.com>
2015-05-29 08:59:19 +0800

    除文档里没有提到的renderedFrameCount外，Time类下的所有属性的文档已补完。

--- UnityEngine/Time.cs
+++ UnityEngine/Time.cs
@@ -17,7 +17,7 @@ namespace UnityEngine
         /// </summary>
         public static int captureFramerate { get; set; }
         /// <summary>
-        /// 完成上一帧所花费的时间，单位秒。
+        /// 完成上一帧所花费的时间，单位秒（只读）。
         /// <para>使用这个函数让你的游戏与帧率无关。</para>
         /// <para>如果你在每一帧增加或者减少某个变量，那么你最好乘上 Time.deltaTime. 当你乘上 Time.deltaTime 时，你所表达的意思是：我想要每秒移动这个物体10米，而不是每帧移动10米。</para>
         /// <para>如果是在 MonoBehaviour 的 FixedUpdate 内调用， 那么返回的是固定帧率的时间差。</para>
@@ -25,13 +25,13 @@ namespace UnityEngine
         /// </summary>
         public static float deltaTime { get { return default(float); } }
         /// <summary>
-        /// 物理系统以及其他固定帧率的更新（像是 MonoBehaviour 的 FixedUpdate）的时间间隔，单位秒。
+        /// 物理系统以及其他固定帧率的更新（例如 MonoBehaviour 的 FixedUpdate）的时间间隔，单位秒。
         /// <para>为了读取增量时间建议使用 Time.deltaTime，因为如果你是在 FixedUpdate 函数或 Update 函数里，那么它自动返回正确的增量时间。</para>
         /// <para>请注意，考虑到游戏内时间，fixedDeltaTime 时间间隔是受 timeScale 的影响。</para>
         /// </summary>
         public static float fixedDeltaTime { get; set; }
         /// <summary>
-        /// 最近一次开始 FixedUpdate 的时间。这是从游戏开始以来的时间，单位为秒。
+        /// 最近一次开始 FixedUpdate 的时间（只读）。这是从游戏开始以来的时间，单位为秒。
         /// <para>固定时间以固定的时间间隔更新（相当于 fixedDeltaTime），直到达到 time 属性。</para>
         /// </summary>
         public static float fixedTime { get { return default(float); } }
@@ -49,11 +49,13 @@ namespace UnityEngine
         /// <summary>
         /// 从游戏开始到现在的实际的时间，单位为秒（只读）。
         /// <para>在几乎所有的情况下，你都能用且应该用 Time.time 代替。</para>
+        /// <para>realtimeSinceStartup 返回不受 Time.timeScale 影响的从开始起计算的时间。即使是玩家暂停住了（丢在后台），realtimeSinceStartup 依然保持增长。当你通过设置 Time.timeScale 为零来使游戏暂停，却仍然想要度量时间时，此时 realtimeSinceStartup 将会很有用。</para>
+        /// <para>请注意，realtimeSinceStartup 返回系统定时器报告的时间。具体取决于平台和硬件，它甚至有可能在多个连续的帧里报告同一个值。如果你想用时间差除别的东西，小心这个问题（时间差可能为零！）。</para>
         /// </summary>
         public static float realtimeSinceStartup { get { return default(float); } }
         public static int renderedFrameCount { get { return default(int); } }
         /// <summary>
-        /// Time.deltaTime 平滑处理后的结果。
+        /// Time.deltaTime 平滑处理后的结果（只读）。
         /// </summary>
         public static float smoothDeltaTime { get { return default(float); } }
         /// <summary>
@@ -61,9 +63,27 @@ namespace UnityEngine
         /// <para>如果在同一帧内调用多次，返回相同的值。当在 MonoBehaviour 的 FixedUpdate 里调用的时候，返回的是 fixedTime 属性。</para>
         /// </summary>
         public static float time { get { return default(float); } }
+        /// <summary>
+        /// 时间流逝速度的缩放比例。该属性可以被用来制作慢镜头特效。
+        /// <para>当 timeScale 为 1.0 时，时间的流逝速度是正常的时间流速。当 timeScale 是 0.5 时，时间的流速是正常时间流速的 1/2.</para>
+        /// <para>当 timeScale 被设置为零时，游戏基本上就暂停了，只要你所有的函数都是帧率无关的。</para>
+        /// <para>timeScale 影响 Time 类的所有 time 和 deltaTime 属性变量，除了 realtimeSinceStartup 。</para>
+        /// <para>如果你降低 timeScale，那么建议你也降低 Time.fixedDeltaTime 同样的数量。</para>
+        /// <para>当 timeScale 被设为零时， 函数 FixedUpdate 将不会被调用</para>
+        /// </summary>
         public static float timeScale { get; set; }
+        /// <summary>
+        /// 此帧开始的时间（只读）。这是从最后一个等级（场景？）加载开始计算的秒数。
+        /// </summary>
         public static float timeSinceLevelLoad { get { return default(float); } }
+        /// <summary>
+        /// 不受 timeScale 影响的完成上一帧所花费的时间，单位秒（只读）。
+        /// </summary>
         public static float unscaledDeltaTime { get { return default(float); } }
+        /// <summary>
+        /// 不受 timeScale 影响的此帧开始的时间。这是从游戏开始起计算的时间，单位为秒。
+        /// <para>如果在同一帧内调用多次，返回相同的值。与 time 不同的是该值不受 timeScale 影响。</para>
+        /// </summary>
         public static float unscaledTime { get { return default(float); } }
     }
 }
