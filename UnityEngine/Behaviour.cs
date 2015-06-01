6b229940189b2f7742bf07237158002229446989
Hoxily <hoxily@qq.com>
2015-06-01 15:25:34 +0800

    修改未翻译的“and”

--- UnityEngine/Behaviour.cs
+++ UnityEngine/Behaviour.cs
@@ -4,7 +4,7 @@ namespace UnityEngine
 {
     /// <summary>
     /// Behaviour 是一种可以控制 激活/失效 的 Component。
-    /// <para>参见：MonoBehaviour and Component.</para>
+    /// <para>参见：MonoBehaviour 和 Component.</para>
     /// </summary>
     public class Behaviour : Component
     {
@@ -13,7 +13,7 @@ namespace UnityEngine
         /// </summary>
         public Behaviour() { }
         /// <summary>
-        /// 激活的 Behaviour 被更新， 而失效的 Behaviour 则不是。
+        /// 激活的 Behaviour 被更新， 而失效的 Behaviour 则不会。
         /// <para>这个属性在 inspector 面板上呈现了一个小的复选框。</para>
         /// </summary>
         public bool enabled { get; set; }
